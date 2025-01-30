dfbaaa0293ef4f0941e2634d7d297eb534130b5b
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-01-30 14:35:36 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,21 @@
-public class Main {
+public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        IdadePessoa pessoa1 = new IdadePessoa();
+        pessoa1.setIdade(15);
+        pessoa1.setNome("João");
+
+        System.out.println(pessoa1.getNome() + " tem " + pessoa1.getIdade() + " anos.");
+        pessoa1.verificarIdade();
+
+
+        IdadePessoa pessoa2 = new IdadePessoa();
+        pessoa2.setIdade(20);
+        pessoa2.setNome("José");
+
+        System.out.println(pessoa2.getNome() + " tem " + pessoa2.getIdade() + " anos.");
+        pessoa2.verificarIdade();
+
+
+
     }
 }
